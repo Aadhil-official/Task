cb7043da77075305ca7c70ac77c3f8c51feff699
Aadhil-official <aadhilmhm.21@uom.lk>
2025-03-25 12:44:26 +0530

    Add test task

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,18 +1,30 @@
-import { Grid, Typography } from '@mui/material'
+import { Button, Grid, Typography } from '@mui/material'
 import React from 'react'
 import cargo2 from '../assets/cargo2.webp'
 
 function Welcome() {
     return (
-        <div style={{marginTop:'20px'}}>
+        <div style={{ marginTop: '20px' }}>
             <Grid container>
                 <Grid item xs={6}>
-                    <img src={cargo2} style={{marginLeft:'20px'}} width='70%' height='90%' alt="cargo img" />
+                    <img src={cargo2} style={{ marginLeft: '20px' }} width='70%' height='90%' alt="cargo img" />
                 </Grid>
                 <Grid item xs={6}>
                     <Typography variant='h4'>
                         TransMax Logistic Around <span style={{ color: 'orange' }}>the world</span>
                     </Typography>
+
+                    <Typography variant='body2'>
+                        TransMax is the world's driving worldwide coordinations supplier - we uphold industry and exchange the world wide trade of merchandise through land transport<br /><br />
+
+                        <span style={{ color: 'gray' }}>
+                            Our worth added administrations guarantee the progression of prouducts proceeds consistently and supply chains stay loan and streamined for progress
+                        </span>
+                    </Typography>
+                    <br /><br />
+                    <Button variant='contained' size='small' sx={{ backgroundColor: 'lightgray', padding: '10px', borderRadius: '0px' }}>
+                        more about us
+                    </Button>
                 </Grid>
             </Grid>
         </div>
